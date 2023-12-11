f2110bf424e03d9e913da30d02a5b901d458031b
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-11 13:37:08 -0300

    Create ReviewController.java

---
+++ src/main/java/com/example/reactspring/controllers/ReviewController.java
@@ -0,0 +1,28 @@
+package com.example.reactspring.controllers;
+
+import com.example.reactspring.models.Review;
+import com.example.reactspring.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping("/reviews/{id}")
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
+    }
+
+}
