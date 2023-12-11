26551c16958366f25ef57876a83ecd3c12dc8f4c
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-11 10:06:44 -0300

    movie class

---
+++ src/main/java/com/example/reactspring/Movie.java
@@ -0,0 +1,19 @@
+package com.example.reactspring;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+public class Movie {
+
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
